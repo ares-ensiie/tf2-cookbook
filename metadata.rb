d7e6caa1d2e4e60c9c6ce3cc8b3a6a0430c2f475
Phil <philigaultier@gmail.com>
2013-10-02 12:24:59 +0200

    Added MIT license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'hlds'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures hlds'
+maintainer       'Ares'
+maintainer_email 'philgaultier@gmail.com'
+license          'MIT License'
+description      'Installs/Configures half-life dedicated server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.6'
